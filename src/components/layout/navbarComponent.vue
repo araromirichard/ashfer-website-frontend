ca11b316b3ccda5e3f558a8577928fbd0343c0d2
karosaxy <araromirichard@gmail.com>
2022-06-11 00:51:32 +0100

    toggle nav

--- src/components/layout/navbarComponent.vue
+++ src/components/layout/navbarComponent.vue
@@ -5,9 +5,7 @@
       class="px-4 py-4 md:py-6 bg-white shadow md:fixed w-full z-10 transition-colors duration-500"
       :class="{ 'md:bg-transparent md:shadow-none': !showWhiteNav }"
     >
-      <div
-        class="container mx-auto flex flex-col md:flex-row item-center transition-all ease-out duration-500 md:transition-none"
-      >
+      <div class="container mx-auto flex flex-col md:flex-row item-center">
         <div
           class="flex flex-row w-full items-center justify-between md:basis-1/3"
         >
@@ -72,15 +70,19 @@
           </div>
         </div>
         <div
-          class="text-blacky mt-4 md:mt-0 w-full md:space-x-10 font-poppins font-bold sm:text-sm md:text-lg md:justify-start md:content-center md:items-center md:flex md:flex-row basis-2/3"
+          class="menu-nav text-blacky mt-4 md:mt-0 w-full md:space-x-10 font-poppins font-bold sm:text-sm md:text-lg md:justify-start md:content-center md:items-center md:flex md:flex-row basis-2/3 transition-all ease-out duration-500 md:transition-none"
           :class="{ hidden: !isVisible, 'md:text-white': !showWhiteNav }"
         >
-          <NavbarLinks to="/" label="Home" />
-          <NavbarLinks to="/about-us" label="About Us" />
-          <NavbarLinks to="/our-business" label="Our Business" />
-          <NavbarLinks to="/our-clients" label="Our Clients" />
-          <NavbarLinks to="/contact" label="Contact " />
-          <NavbarLinks to="/blog" label="Blog" />
+          <NavbarLinks @click="toggle" to="/" label="Home" />
+          <NavbarLinks @click="toggle" to="/about-us" label="About Us" />
+          <NavbarLinks
+            @click="toggle"
+            to="/our-business"
+            label="Our Business"
+          />
+          <NavbarLinks @click="toggle" to="/our-clients" label="Our Clients" />
+          <NavbarLinks @click="toggle" to="/contact" label="Contact " />
+          <NavbarLinks @click="toggle" to="/blog" label="Blog" />
         </div>
       </div>
     </nav>
@@ -118,4 +120,13 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.menu-nav {
+  transform: translateY(0);
+  transition: transform 0.3s linear;
+}
+
+.menu-nav {
+  transform: translateY(-100%);
+}
+</style>
