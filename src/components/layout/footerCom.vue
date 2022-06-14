8fc7c742e0b570426391f9e5d4303cd640ea3c7a
karosaxy <araromirichard@gmail.com>
2022-06-14 21:57:49 +0100

    correction-footer

--- src/components/layout/footerCom.vue
+++ src/components/layout/footerCom.vue
@@ -75,7 +75,7 @@
           >
 
           <ul>
-            <li class="links text-xs md:text-sm flex">
+            <li class="links text-base md:text-sm flex">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-5 w-5"
